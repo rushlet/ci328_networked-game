517dc40ec3ef55f346527eae4c9ac2f9cd6d0a4d
Cai Lehwald <cailehwald@googlemail.com>
2018-02-22 15:54:33 +0000

    V0.1.2
    Further Refactor Client code to abstract out method calls in the constructor.
    Fixed bug with socket losing definition with the allPlayers call.
    Server and Client now communicate as original example with no errors

--- client/js/client.js
+++ client/js/client.js
@@ -3,23 +3,36 @@ class Client {
   constructor() {
     console.log("in client constructor");
     this.socket = io('http://localhost:55000');
+    this.newPlayer();
+    this.allPlayers();
+  }
 
+  newPlayer() {
     this.socket.on('newplayer', function(data) {
       addNewPlayer(data.id, data.x, data.y);
     });
+  }
 
+  allPlayers() {
+    let client = this;
     this.socket.on('allplayers', function(data) {
       for (var i = 0; i < data.length; i++) {
         addNewPlayer(data[i].id, data[i].x, data[i].y);
       }
+      client.move();
+      client.remove();
+    });
+  }
 
-      this.socket.on('move', function(data) {
-        movePlayer(data.id, data.x, data.y);
-      });
+  move() {
+    this.socket.on('move', function(data) {
+      movePlayer(data.id, data.x, data.y);
+    });
+  }
 
-      this.socket.on('remove', function(id) {
-        removePlayer(id);
-      });
+  remove() {
+    this.socket.on('remove', function(id) {
+      removePlayer(id);
     });
   }
 
