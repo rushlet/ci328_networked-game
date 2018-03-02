87bfb14193eb3fd66e9fc2c54185fbddc1a3cfdf
Becky Rush <becky.rush14@yahoo.co.uk>
2018-03-02 13:18:49 +0000

    initial keyboard input

--- client/js/GameController.js
+++ client/js/GameController.js
@@ -8,7 +8,8 @@ function main() {
   // Initialize the phaser game window, give it a width of GAMEWIDTH and a height of GAMEHEIGHT, set the rendering context to auto and attach the window to a div with the ID "GameWindow"
   game = new Phaser.Game(GAMEWIDTH, GAMEHEIGHT, Phaser.AUTO, 'GameWindow', {
     preload: preload,
-    create: create
+    create: create,
+    update: update
   });
 }
 
@@ -28,8 +29,25 @@ function create() {
   console.log("Testing Server Connection");
   client.sendTest();
 
-  game.input.onTap.add(getCoordinates, this);
+  // game.input.onTap.add(getCoordinates, this);
+  game.cursors = game.input.keyboard.createCursorKeys();
+
   client.askNewPlayer();
+}
+
+function update() {
+  if (game.cursors.left.isDown) {
+          client.sendCursor("left");
+     }
+     else if (game.cursors.right.isDown) {
+         client.sendCursor("right");
+     }
+     else if (game.cursors.up.isDown) {
+         client.sendCursor("up");
+     }
+     else if (game.cursors.down.isDown) {
+         client.sendCursor("down");
+     }
 }
 
 function getCoordinates(pointer) {
