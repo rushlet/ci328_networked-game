48032166822934cfaacd329389fd75cdbc54dd82
Becky Rush <becky.rush14@yahoo.co.uk>
2018-04-24 21:00:35 +0100

    implement powerup collectible and collision, need to add powerup effects. Generalise generateDots function to generateEntity, strengthen player collision check

--- server/gameWorld.js
+++ server/gameWorld.js
@@ -1,5 +1,5 @@
 var tilemapper = require('./utils/tilemap-array-generator.js');
-module.exports = class gameWorld {
+module.exports = class GameWorld {
 
   constructor() {
     this.tilemap = tilemapper.create2dArrayFromTilemap(0);
@@ -9,16 +9,19 @@ module.exports = class gameWorld {
       dots: {},
       powerups: {}
     };
+    this.powerups = ['halfSpeed', 'doubleSpeed', 'halfPoints', 'doublePoints'];
   }
 
   gamePrep(io, client) {
     this.chooseHero();
-    this.generateDots();
+    this.generateEntity('dots', 5);
+    this.generateEntity('powerups', 1);
     io.emit('drawDots', this.getArrayOfEntityType('dots'));
     io.emit('updateHero', this.getArrayOfEntityType('players'));
     io.emit('addUI', this.getArrayOfEntityType('players'), client.user.id);
     io.emit('startGame');
     this.startGameTimer(io);
+    this.addPowerups(io);
   }
 
   chooseHero() {
@@ -27,10 +30,10 @@ module.exports = class gameWorld {
     this.entities.players[hero].hero = true;
   }
 
-  generateDots() {
-    for (var i = 0; i < 5; i++) {
+  generateEntity(type, quantity) {
+    for (var i = 0; i < quantity; i++) {
       var location = this.initialEntityPosition(this.tilemap);
-      this.createEntity('dots', i, location.worldX, location.worldY);
+      this.createEntity(type, i, location.worldX, location.worldY);
     }
   }
 
@@ -41,7 +44,10 @@ module.exports = class gameWorld {
       x: x,
       y: y
     }
-    if (type === "players") {
+    if (type === "powerups") {
+      this.entities[type][id]["visible"] = false;
+    }
+    else if (type === "players") {
       this.entities[type][id]["direction"] = "";
       this.entities[type][id]["expectedPosition"] = {
         x: x,
@@ -49,8 +55,15 @@ module.exports = class gameWorld {
       };
       this.entities[type][id]["hero"] = false;
       this.entities[type][id]["score"] = 0;
+      this.entities[type][id]["powerups"] = {
+        halfSpeed: false,
+        doubleSpeed: false,
+        halfPoints: false,
+        doublePoints: false
+      };
     }
   }
+
   randomInt(low, high) {
     return Math.floor(Math.random() * (high - low) + low);
   }
@@ -71,16 +84,27 @@ module.exports = class gameWorld {
               entities[type][id].y = location.worldY;
               io.emit('updateDots', gameWorld.getArrayOfEntityType('dots'));
               entities.players[player.id].score += 1;
-            } else if (entities[type][id].hero && !player.hero) {
+            } else if (type == 'players' && entities[type][id].hero && !player.hero) {
               entities[type][id].hero = false;
               entities[type][player.id].hero = true;
               entities[type][player.id].score += 3;
               io.emit('updateHero', gameWorld.getArrayOfEntityType('players'));
-            } else if (!entities[type][id].hero && player.hero) {
+            } else if (type == 'players' && !entities[type][id].hero && player.hero) {
               entities[type][id].hero = true;
               entities[type][id].score += 3;
               entities[type][player.id].hero = false;
               io.emit('updateHero', gameWorld.getArrayOfEntityType('players'));
+            } else if (type == "powerups" && entities.powerups[0].visible) {
+              console.log('power up collision if');
+              // select random powerup
+              let selectedPowerup = gameWorld.powerups[gameWorld.randomInt(0, 4)];
+              // add relevant boolean to player
+              entities.players[player.id].powerups[selectedPowerup] = true;
+              io.emit('powerupCaught', selectedPowerup);
+              let powerupExpire = setTimeout(() => {
+                io.emit('powerupExpire');
+                clearTimeout(powerupExpire);
+              }, 2000);
             }
             client.emit('updateScore', player.score);
             io.emit('updateOtherScores', gameWorld.getArrayOfEntityType('players'));
@@ -130,7 +154,31 @@ module.exports = class gameWorld {
     io.emit('startGameTimer', duration);
     this.gameOverTimer = setTimeout(() => {
       io.emit('endGame', duration);
-      clearTimeout(this.gameOverTimer);
+      this.stopTimers();
     }, duration);
+  }
+
+  addPowerups(io) {
+    let duration = 10000;
+    console.log(this.entities.powerups);
+    io.emit('addPowerup', this.entities.powerups[0].x, this.entities.powerups[0].y);
+    this.powerupTimer = setInterval(() => {
+      if (this.entities.powerups[0].visible) {
+        this.entities.powerups[0].visible = false;
+        let location = this.initialEntityPosition(this.tilemap)
+        this.entities.powerups[0].x = location.worldX;
+        this.entities.powerups[0].y = location.worldY;
+        console.log(this.entities.powerups[0]);
+      } else {
+        this.entities.powerups[0].visible = true;
+        console.log(this.entities.powerups[0]);
+      }
+      io.emit('updatePowerup', this.entities.powerups[0].visible, this.entities.powerups[0].x, this.entities.powerups[0].y);
+    }, duration);
+  }
+
+  stopTimers() {
+    clearTimeout(gameWorld.gameOverTimer);
+    clearInterval(gameWorld.powerupTimer);
   }
 }
